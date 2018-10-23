7707f3da02bdc045df635d4c281ae421ffcee3b9
fuzzy <fuzzy.nova.goblin@outlook.com>
2018-10-23 16:50:01 -0400

    made the text bigger

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,9 @@ import 'dart:async';
 
 void main()
 {
-  Duration duration = Duration(seconds: 1);
-
-//  Timer.periodic(duration, (Timer t){
-//    print("hi");
-////    Conversion.h = DateTime.now().hour;
-////    Conversion.m = DateTime.now().minute;
-////    Conversion.s = DateTime.now().second;
-////    print(Conversion.h.toString() + ":" + Conversion.m.toString() + ":" + Conversion.s.toString());
-////    Conversion.timeText = Conversion.h.toString() + ":" + Conversion.m.toString() + ":" + Conversion.s.toString();
-////
-////    Conversion.h = Conversion.toHexValue(Conversion.h);
-////    Conversion.m = Conversion.toHexValue(Conversion.m);
-////    Conversion.s = Conversion.toHexValue(Conversion.s);
-//  });
-
-  runApp(new MaterialApp(home: HomePage(), title: "Clock",));
-  
+  runApp(new MaterialApp(
+    home: HomePage(),
+    title: "Clock",
+    color: Colors.white,
+  ));
 }
